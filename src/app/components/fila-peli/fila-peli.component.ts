7aa458e255e18a7263c50a266d0c60c8bf25aafd
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-02-09 17:00:03 +0100

    Intermedio

--- src/app/components/fila-peli/fila-peli.component.ts
+++ src/app/components/fila-peli/fila-peli.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pelicula } from '../../interfaces/pelicula';
 
 @Component({
   selector: 'app-fila-peli',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './fila-peli.component.css'
 })
 export class FilaPeliComponent {
-
+  @Input() pelicula:Pelicula={};
 }
--- src/app/components/listado-pelis/listado-pelis.component.ts
+++ src/app/components/listado-pelis/listado-pelis.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FilaPeliComponent } from "../fila-peli/fila-peli.component";
+import { GestorpeliculasService } from '../../services/gestorpeliculas.service';
+import { Pelicula } from '../../interfaces/pelicula';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-listado-pelis',
-  standalone: true,
-  imports: [],
-  templateUrl: './listado-pelis.component.html',
-  styleUrl: './listado-pelis.component.css'
+    selector: 'app-listado-pelis',
+    standalone: true,
+    templateUrl: './listado-pelis.component.html',
+    styleUrl: './listado-pelis.component.css',
+    imports: [FilaPeliComponent, CommonModule]
 })
 export class ListadoPelisComponent {
-
+    listaPeliculas:Pelicula[];
+    constructor(private servicioPeliculas:GestorpeliculasService){
+        this.listaPeliculas=servicioPeliculas.getPeliculas();   
+    }
 }

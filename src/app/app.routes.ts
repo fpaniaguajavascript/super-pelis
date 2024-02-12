8a143e5fb7cfcfad336b4db43d8f325e4439fb1c
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-02-12 17:51:25 +0100

    Listado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { FichaPeliComponent } from './components/ficha-peli/ficha-peli.component
 import { PrincipalComponent } from './components/principal/principal.component';
 import { Page404Component } from './components/page404/page404.component';
 import { AboutComponent } from './components/about/about.component';
+import { EditorComponent } from './components/editor/editor.component';
 
 export const routes: Routes = [
+    {path:'editor',component:EditorComponent},
     {path:'about',component:AboutComponent},
     {path:'acercade',redirectTo:'about'},
     {path:'editar/:titulo',component:FichaPeliComponent},
     {path:'', component:PrincipalComponent},//Raiz
-    {path:'**',component:Page404Component}//Cualquier petición no gestionada antes acaba aquí
+    {path:'**',component:Page404Component},//Cualquier petición no gestionada antes acaba aquí
 ];
--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { GestorpeliculasService } from '../../services/gestorpeliculas.service';
+import { Pelicula } from '../../interfaces/pelicula';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
 export class EditorComponent {
+  listaPeliculas:Pelicula[];
+  constructor(private gps:GestorpeliculasService){
+    this.listaPeliculas = this.gps.getPeliculas();
+  }
 
 }

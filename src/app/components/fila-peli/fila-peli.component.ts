13699185708e3928043c96295c30ac1572caa302
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-02-12 20:16:53 +0100

    Fin

--- src/app/components/fila-peli/fila-peli.component.ts
+++ src/app/components/fila-peli/fila-peli.component.ts
@@ -2,17 +2,25 @@ import { Component, Input } from '@angular/core';
 import { Pelicula } from '../../interfaces/pelicula';
 import { GestorpeliculasService } from '../../services/gestorpeliculas.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fila-peli',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, CommonModule ],
   templateUrl: './fila-peli.component.html',
   styleUrl: './fila-peli.component.css'
 })
 export class FilaPeliComponent {
   @Input() pelicula:Pelicula={};
+  estilos:string[]=[];
   constructor(private gestorPeliculas:GestorpeliculasService){}
+  public ngOnInit(){
+    if (this.pelicula.anyo!<2000){
+      this.estilos.push("descatalogada");
+      this.estilos.push("cursiva");
+    }
+  }
   borrarPeli(titulo:string | undefined){
     this.gestorPeliculas.eliminarPelicula(titulo);
   }
--- src/app/interfaces/pelicula.ts
+++ src/app/interfaces/pelicula.ts
@@ -2,4 +2,5 @@ export interface Pelicula {
     titulo?:string;
     imagen?:string;
     anyo?:number;
+    enCartelera?:boolean;
 }
